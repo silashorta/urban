78e8f343dccfab41fbaddb1285d88e2f2ca34a49
silashorta <92862835+silashorta@users.noreply.github.com>
2024-04-03 23:39:44 -0300

    up edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,99 @@ import { CiSearch } from "react-icons/ci";
 import { FaUser, FaUserFriends } from "react-icons/fa";
 import { BiBuildingHouse } from "react-icons/bi";
 import { FaEdit } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
+import { TbHomeMinus } from "react-icons/tb";
+import Modal from './Components/Modal';
+import { FaRegSquareMinus } from "react-icons/fa6";
+import { FaRegPlusSquare } from "react-icons/fa";
+
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [moradores, setMoradores] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedUnidade, setSelectedUnidade] = useState(null);
+  const [newDependente, setNewDependente] = useState({ nome: '', tipo: '' });
+
+  const toggleModal = (index) => {
+    setSelectedUnidade(index);
+    setIsOpen(!isOpen);
+  };
+
+console.log(moradores[selectedUnidade])
+
+const submitAlteracoes = async () => {
+  try {
+    const moradorId = moradores[selectedUnidade].id;
+
+    // Mapeia os dependentes para incluí-los no objeto moradorAtualizado
+    const dependentesAtualizados = moradores[selectedUnidade].Dependentes.map(dependente => ({
+      nome: dependente.nome,
+      tipo: dependente.tipo
+    }));
+
+    console.log(dependentesAtualizados)
+
+    // Cria o objeto moradorAtualizado com os dados atualizados do morador e seus dependentes
+    const moradorAtualizado = {
+      nome: moradores[selectedUnidade].nome,
+      tipo: moradores[selectedUnidade].tipo,
+      bloco: moradores[selectedUnidade].bloco,
+      apartamento: moradores[selectedUnidade].apartamento,
+      Dependentes: dependentesAtualizados // Inclui os dependentes atualizados no objeto
+    };
+
+    console.log(moradorAtualizado)
+
+    
+    const response = await fetch(`http://silashortadev.com.br/morador/${moradorId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moradorAtualizado), // Envia os dados atualizados do morador, incluindo os dependentes
+    });
+    
+    const data = await response.json();
+    console.log('Dados atualizados:', data);
+    setIsOpen(false); // Fecha o modal após o envio bem-sucedido
+  } catch (error) {
+    console.error('Erro ao enviar dados:', error);
+  }
+};
+
+
+  const handleInputChange = (e, field) => {
+    const value = e.target.value;
+    const updatedMoradores = [...moradores];
+    updatedMoradores[selectedUnidade][field] = value;
+    setMoradores(updatedMoradores);
+  };
+
+  // Função para adicionar um novo dependente
+  const addDependente = () => {
+    const updatedMoradores = [...moradores];
+    updatedMoradores[selectedUnidade].Dependentes.push(newDependente);
+    setMoradores(updatedMoradores);
+    // Limpa os campos de entrada do novo dependente após adicioná-lo
+    setNewDependente({ nome: '', tipo: '' });
+  };
+
+  // Função para remover um dependente
+  const removeDependente = (dependenteIndex) => {
+    const updatedMoradores = [...moradores];
+    updatedMoradores[selectedUnidade].Dependentes.splice(dependenteIndex, 1);
+    setMoradores(updatedMoradores);
+  };
+
+  // Função para lidar com a alteração dos dados dos dependentes
+  const handleDependenteChange = (e, dependenteIndex, field) => {
+    const { value } = e.target;
+    const updatedMoradores = [...moradores];
+    updatedMoradores[selectedUnidade].Dependentes[dependenteIndex][field] = value;
+    setMoradores(updatedMoradores);
+  };
+
+
 
   useEffect(() => {
     fetch('http://silashortadev.com.br/morador')
@@ -19,27 +107,23 @@ function App() {
         setMoradores(data);
       })
       .catch(error => console.error('Erro ao buscar moradores:', error));
-      
-    }, []);
-    
-    const handleSearch = (e) => {
-      const searchTerm = e.target.value.toLowerCase();
-      setSearchTerm(searchTerm);
-    };
-    
-    const filteredData = moradores.filter(item => {
-      // Se o termo de pesquisa estiver vazio, mostrar toda a lista
-      if (!searchTerm) return true;
-      
-      // Verificar se o nome, o número do apartamento ou o nome dos dependentes contêm o termo de pesquisa
+  }, []);
 
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+  };
+
+  const filteredData = moradores.filter(item => {
+    // Se o termo de pesquisa estiver vazio, mostrar toda a lista
+    if (!searchTerm) return true;
+  
+    // Verificar se o nome, o número do apartamento ou o nome dos dependentes contêm o termo de pesquisa
     return item.nome.toLowerCase().includes(searchTerm) ||
       (item.apartamento && item.apartamento.toString().includes(searchTerm)) ||
-      (item.dependentes && item.dependentes.length > 0 && item.dependentes.some(dependente => dependente.nome.toLowerCase().includes(searchTerm)));
+      (item.Dependentes && item.Dependentes.length > 0 && item.Dependentes.some(dependente => dependente.nome.toLowerCase().includes(searchTerm)));
   });
   
-
-
 
   return (
     <div>
@@ -58,10 +142,10 @@ function App() {
             <CiSearch />
           </div>
         </div>
-        {filteredData && filteredData.length === 0 ? (
+        {filteredData.length === 0 ? (
           <div className="min-h-screen text-gray-600 text-xl text-center">Nenhum registro encontrado.</div>
         ) : (
-          filteredData && filteredData.map((item, index) => (
+          filteredData.map((item, index) => (
             <div key={index} className='flex flex-col gap-2 m-2 p-4 lg:p-8 bg-gray-50 rounded-lg'>
               <div className='flex gap-6'>
                 <div className='flex gap-1'>
@@ -110,11 +194,83 @@ function App() {
                 </div>
               ) : null}
               <div className='flex gap-4'>
-                <FaEdit className='text-2xl cursor-pointer text-gray-800 hover:text-amber-600 duration-300 hover:scale-110' />
-                <MdDelete className='text-2xl cursor-pointer text-gray-800 hover:text-red-600 duration-300 hover:scale-110' />
+                <button onClick={() => toggleModal(index)}>
+                  <FaEdit className='text-2xl cursor-pointer text-gray-800 hover:text-amber-600 duration-300 hover:scale-110' />
+                </button>
+                <TbHomeMinus className='text-2xl  font-bold cursor-pointer text-gray-800 hover:text-red-600 duration-300 hover:scale-110' />
               </div>
             </div>
           ))
+        )}
+        {isOpen && selectedUnidade !== null && (
+          <Modal isOpen={isOpen} onClose={toggleModal}>
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3  sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Editar Unidade {filteredData[selectedUnidade].apartamento} - Bloco {filteredData[selectedUnidade].bloco}</h3>
+                <div className="mt-2">
+                  <div className="flex flex-col gap-1 text-sm text-gray-500">
+                    <div>
+                      <input type='text'
+                        value={filteredData[selectedUnidade].nome}
+                        onChange={(e) => handleInputChange(e, 'nome')}
+                        placeholder='Nome'
+                        className='w-full border p-1 text-gray-800 rounded' />
+                    </div>
+                    <div>
+                      <input type='text'
+                        value={filteredData[selectedUnidade].tipo}
+                        onChange={(e) => handleInputChange(e, 'tipo')}
+                        placeholder='Tipo'
+                        className='w-full border p-1 text-gray-800 rounded' />
+                    </div>
+                    {/* Adicione outras informações conforme necessário */}
+                  </div>
+                  {filteredData[selectedUnidade].Dependentes.map((dependente, dependenteIndex) => (
+                    <div key={dependenteIndex} className="mt-2">
+                      <div className="flex items-center gap-6 text-sm text-gray-500">
+                        <div className="flex flex-col gap-1 bg-slate-50 p-2 rounded">
+                          <div className="flex items-center gap-2">
+                            <div className="text-black">Nome:</div>
+                            <input
+                              type="text"
+                              className="p-1 border rounded bg-slate-50 uppercase"
+                              value={dependente.nome}
+                              onChange={(e) => handleDependenteChange(e, dependenteIndex, 'nome')}
+                            />
+                          </div>
+                          <div className="flex items-center gap-2">
+                            <div className="text-black">Tipo:</div>
+                            <input
+                              type="text"
+                              className="p-1 border rounded bg-slate-50 uppercase"
+                              value={dependente.tipo}
+                              onChange={(e) => handleDependenteChange(e, dependenteIndex, 'tipo')}
+                            />
+                          </div>
+                        </div>
+                        <div
+                          className="text-red-500 hover:text-red-700 text-xl hover:scale-110 hover:cursor-pointer duration-200"
+                          onClick={() => removeDependente(dependenteIndex)}
+                        >
+                          <FaRegSquareMinus />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                  <div>
+                    <FaRegPlusSquare
+                      className="text-slate-800 hover:text-black text-2xl hover:cursor-pointer hover:scale-110 duration-200 my-4"
+                      onClick={addDependente}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button type='submit' onClick={submitAlteracoes}
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm my-4 px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
+              Salvar Alterações
+            </button>
+          </Modal>
         )}
       </section>
     </div>
--- src/Components/Modal/index.jsx
+++ src/Components/Modal/index.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function Modal() {
-    return (
-        <div>
-            <div id="default-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative p-4 w-full max-w-2xl max-h-full">
-                    
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Terms of Service
-                            </h3>
-                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        
-                        <div class="p-4 md:p-5 space-y-4">
-                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                            </p>
-                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                            </p>
-                        </div>
-                       
-                        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                            <button data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
-                        </div>
-                    </div>
-                </div>
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+          <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              {children}
             </div>
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                onClick={onClose}
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
